f8f4c46d39ed08ecd720dcd1b128d8b8810b5bc2
Dius Montenegro <114764373+DreaUltimate@users.noreply.github.com>
2023-02-15 10:37:33 +0800

    Add code to make the code reusable
    I've defined a function called convert_pdf_to_csv() that takes the input file and output file names as arguments. The function performs the same operations as the previous code, but it does so in a reusable and modular way.
    
    I've also updated the logging statements to include the file names, so it's easier to track which files have been processed and which ones had errors.
    
    Finally, to convert multiple files, you can simply call the convert_pdf_to_csv() function for each file you need to convert, passing in the appropriate input and output file names.
    
    With these changes, the code is now more reusable and can be easily adapted to handle multiple files without duplicating code.

--- PDF2CSV/pdf2csv.py
+++ PDF2CSV/pdf2csv.py
@@ -5,21 +5,24 @@ import logging
 logging.basicConfig(filename='tabula.log', level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s: %(message)s')
 
-try:
-    # Convert PDF to CSV using tabula
-    tabula.convert_into("samplecsv.pdf", "samplecsv.csv", output_format="csv", pages="all")
+def convert_pdf_to_csv(input_file, output_file):
+    try:
+        # Convert PDF to CSV using tabula
+        tabula.convert_into(input_file, output_file, output_format="csv", pages="all")
+    except FileNotFoundError:
+        # Handle file not found errors
+        logging.error(f"Error: File not found - {input_file}")
+        print(f"Error: File not found - {input_file}")
+    except Exception as e:
+        # Handle other errors
+        logging.error(f"Error: {e}")
+        print(f"Error: {e}")
+    else:
+        # Handle successful conversion
+        logging.info(f"{input_file} successfully converted to {output_file}")
+        print(f"{input_file} successfully converted to {output_file}")
 
-except FileNotFoundError:
-    # Handle file not found errors
-    logging.error("Error: File not found")
-    print("Error: File not found")
-except Exception as e:
-    # Handle other errors
-    logging.error(f"Error: {e}")
-    print(f"Error: {e}")
-else:
-    # Handle successful conversion
-    logging.info("PDF successfully converted to CSV")
-    print("PDF successfully converted to CSV")
-
-
+# Call the function for each file you need to convert
+convert_pdf_to_csv("samplecsv1.pdf", "samplecsv1.csv")
+convert_pdf_to_csv("samplecsv2.pdf", "samplecsv2.csv")
+convert_pdf_to_csv("samplecsv3.pdf", "samplecsv3.csv")
