5aef6c1fb9d67cb943e2e381efee14bac77f492b
Dius Montenegro <114764373+DreaUltimate@users.noreply.github.com>
2023-02-16 17:17:31 +0800

    Add captcha image generation and language detection using machine learning
    This commit adds a captcha image generation using the ImageCaptcha library and language detection using the langdetect library. The user is prompted to enter a phone number and a captcha code. If the captcha code is correct, the code uses the phone numbers library to parse the phone number and extract the country of origin, and the langdetect library to detect the language of the input phone number. The detected language and country of origin of the phone number are printed on the console. If the captcha code is incorrect, an error message is printed asking the user to try again.
    
    This update improves the security of the application by preventing automated spam and fraud attacks and enhances the user experience by providing a more interactive and engaging input form.

--- RCG/RCG.py
+++ RCG/RCG.py
@@ -1,14 +1,36 @@
+import phonenumbers     # import phonenumbers library
+from phonenumbers import PhoneNumber, geocoder    # import necessary functions from phonenumbers library
+from langdetect import detect   # import detect function from langdetect library
+from captcha.image import ImageCaptcha  # import ImageCaptcha for generating captcha image
+import string   # import string library for generating random text
+import random   # import random library for generating random text
 
-from captcha.image import ImageCaptcha
-import string
-import random
-
+# Generate a random string for the captcha image
 N = 7
-res = ''.join(random.choices(string.ascii_uppercase + string.digits, k = N))
-str1 = str(res)
-image = ImageCaptcha(width = 300, height = 100)
-captcha_text = str1
+captcha_text = ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
+
+# Generate the captcha image and save it to a file
+image = ImageCaptcha(width=300, height=100)
 data = image.generate(captcha_text)
-image.write(captcha_text, 'C:\\Users\\admin\\Documents\\Python\\Projects\\RCG\\CAPTCHA.png')
+image.write(captcha_text, 'CAPTCHA.png')
+
+# Display the captcha image to the user
 from PIL import Image
-Image.open('C:\\Users\\admin\\Documents\\Python\\Projects\\RCG\\CAPTCHA.png')
+Image.open('CAPTCHA.png').show()
+
+# Prompt the user to enter a phone number and the captcha code
+input_number = input("Enter the phone number: ")
+captcha_code = input("Enter the CAPTCHA code: ")
+
+# Verify the captcha code
+if captcha_code == captcha_text:
+    # If the captcha is correct, detect the language of the input phone number
+    language = detect(input_number)
+    # Parse the phone number and extract the country of origin
+    parsed_number = phonenumbers.parse(input_number)
+    country = geocoder.description_for_number(parsed_number, 'en')
+    # Print the detected language and country of origin of the phone number
+    print(f"The phone number is in {language} and is from {country}")
+else:
+    # If the captcha is incorrect, print an error message
+    print("Incorrect CAPTCHA code. Please try again.")
