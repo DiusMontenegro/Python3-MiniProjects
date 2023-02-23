6458b3b29367c8332c9a8bbf213df5a6b1352aed
Dius Montenegro <114764373+DreaUltimate@users.noreply.github.com>
2023-02-23 21:57:39 +0800

    Added a simple calculator app with GUI using Python and Tkinter.
     Implemented a simple calculator app with a graphical user interface using the Tkinter module in Python. The calculator allows users to perform basic arithmetic operations such as addition, subtraction, multiplication, and division. It also has a "C" button to clear the current calculation and an "=" button to evaluate the expression and display the result.

---
+++ Calculator/calculator.py
@@ -0,0 +1,50 @@
+import tkinter as tk
+
+class Calculator:
+    def __init__(self, master):
+        self.master = master
+        master.title("Calculator")
+
+        self.display = tk.Entry(master, width=30, borderwidth=5)
+        self.display.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+
+        self.create_button("1", 1, 0)
+        self.create_button("2", 1, 1)
+        self.create_button("3", 1, 2)
+        self.create_button("4", 2, 0)
+        self.create_button("5", 2, 1)
+        self.create_button("6", 2, 2)
+        self.create_button("7", 3, 0)
+        self.create_button("8", 3, 1)
+        self.create_button("9", 3, 2)
+        self.create_button("0", 4, 1)
+
+        self.create_button("+", 1, 3)
+        self.create_button("-", 2, 3)
+        self.create_button("*", 3, 3)
+        self.create_button("/", 4, 3)
+        self.create_button("C", 4, 0)
+        self.create_button("=", 5, 2)
+
+    def create_button(self, text, row, col):
+        button = tk.Button(self.master, text=text, padx=40, pady=20,
+                           command=lambda: self.button_click(text))
+        button.grid(row=row, column=col)
+
+    def button_click(self, text):
+        if text == "C":
+            self.display.delete(0, tk.END)
+        elif text == "=":
+            try:
+                result = eval(self.display.get())
+                self.display.delete(0, tk.END)
+                self.display.insert(0, str(result))
+            except:
+                self.display.delete(0, tk.END)
+                self.display.insert(0, "Error")
+        else:
+            self.display.insert(tk.END, text)
+
+root = tk.Tk()
+my_calc = Calculator(root)
+root.mainloop()
