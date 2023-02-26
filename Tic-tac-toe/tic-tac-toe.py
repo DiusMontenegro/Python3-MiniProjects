e84ccdde6f80f96d728aaad3aeae3df8a4e7e900
Dius Montenegro <114764373+DreaUltimate@users.noreply.github.com>
2023-02-26 20:01:06 +0800

    Update tictactoe

--- Tic-tac-toe/tic-tac-toe.py
+++ Tic-tac-toe/tic-tac-toe.py
@@ -7,6 +7,25 @@ current_player = "X"
 player1_score = 0
 player2_score = 0
 
+# create the main window
+window = tk.Tk()
+window.title("Tic Tac Toe")
+
+# create the labels for displaying scores and current turn
+score_label = tk.Label(window, text="Player 1: {}  Player 2: {}".format(player1_score, player2_score))
+score_label.pack()
+turn_label = tk.Label(window, text="Current Turn: Player {}".format(current_player))
+turn_label.pack()
+
+# create the buttons for the game board
+button_frame = tk.Frame(window)
+button_frame.pack()
+buttons = []
+for i in range(9):
+    button = tk.Button(button_frame, text="", width=3, height=1, command=lambda pos=i: on_click(pos))
+    button.grid(row=i // 3, column=i % 3)
+    buttons.append(button)
+
 # define GUI functions
 def on_click(position):
     global current_player
@@ -39,41 +58,38 @@ def switch_player():
 
 def update_board():
     # update the GUI with the current state of the board
-    pass
+    for i, mark in enumerate(board):
+        buttons[i].configure(text=mark)
 
 def check_winner():
     # check for a winning combination and return True if found
-    pass
+    winning_combinations = [
+        (0, 1, 2), (3, 4, 5), (6, 7, 8),  # rows
+        (0, 3, 6), (1, 4, 7), (2, 5, 8),  # columns
+        (0, 4, 8), (2, 4, 6)             # diagonals
+    ]
+    for combo in winning_combinations:
+        if board[combo[0]] == board[combo[1]] == board[combo[2]] != "":
+            return True
+    return False
 
 def display_winner():
     # display a message declaring the winner and update the score
-    pass
+    winner = "Player 1" if current_player == "O" else "Player 2"
+    score_label.configure(text="Player 1: {}  Player 2: {}".format(player1_score, player2_score))
+    tk.messagebox.showinfo("Winner!", "{} wins!".format(winner))
 
 def check_tie():
     # check if the game is tied and return True if it is
-    pass
+    return not "" in board
 
 def display_tie():
     # display a message declaring a tie game
-    pass
+    tk.messagebox.showinfo("Tie Game!", "The game ended in a tie.")
 
 def reset_game():
     global board
     board = ["", "", "", "", "", "", "", "", ""]
     update_board()
-    display_turn()
+   
 
-def display_turn():
-    # display the current player's turn
-    pass
-
-def display_scores():
-    # display the scores for both players
-    pass
-
-def display_game():
-    # display the game board and other UI elements
-    pass
-
-# start the game
-display_game()
