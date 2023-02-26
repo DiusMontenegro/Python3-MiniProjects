532fbf23ebca78644cef49c5da5d96c2ffe33f3b
Dius Montenegro <114764373+DreaUltimate@users.noreply.github.com>
2023-02-26 20:37:49 +0800

    Update tictactoe

--- Tic-tac-toe/tic-tac-toe.py
+++ Tic-tac-toe/tic-tac-toe.py
@@ -1,5 +1,6 @@
 # import necessary libraries
 import tkinter as tk
+from tkinter import messagebox
 
 # define game board and other variables
 board = ["", "", "", "", "", "", "", "", ""]
@@ -12,9 +13,11 @@ window = tk.Tk()
 window.title("Tic Tac Toe")
 
 # create the labels for displaying scores and current turn
-score_label = tk.Label(window, text="Player 1: {}  Player 2: {}".format(player1_score, player2_score))
+score_label = tk.Label(window, text="Player 1: {}  Player 2: {}".format(
+    player1_score, player2_score))
 score_label.pack()
-turn_label = tk.Label(window, text="Current Turn: Player {}".format(current_player))
+turn_label = tk.Label(
+    window, text="Current Turn: Player {}".format(current_player))
 turn_label.pack()
 
 # create the buttons for the game board
@@ -22,11 +25,16 @@ button_frame = tk.Frame(window)
 button_frame.pack()
 buttons = []
 for i in range(9):
-    button = tk.Button(button_frame, text="", width=3, height=1, command=lambda pos=i: on_click(pos))
+    button = tk.Button(button_frame, text="", width=3,
+                       height=1, command=lambda pos=i: on_click(pos))
     button.grid(row=i // 3, column=i % 3)
     buttons.append(button)
 
+
+
 # define GUI functions
+
+
 def on_click(position):
     global current_player
     global board
@@ -41,12 +49,11 @@ def on_click(position):
             else:
                 player2_score += 1
             display_winner()
-            reset_game()
         elif check_tie():
             display_tie()
-            reset_game()
         else:
             switch_player()
+
 
 def switch_player():
     global current_player
@@ -56,10 +63,12 @@ def switch_player():
         current_player = "X"
     display_turn()
 
+
 def update_board():
     # update the GUI with the current state of the board
     for i, mark in enumerate(board):
         buttons[i].configure(text=mark)
+
 
 def check_winner():
     # check for a winning combination and return True if found
@@ -73,23 +82,55 @@ def check_winner():
             return True
     return False
 
+
 def display_winner():
     # display a message declaring the winner and update the score
+    global player1_score, player2_score
     winner = "Player 1" if current_player == "O" else "Player 2"
-    score_label.configure(text="Player 1: {}  Player 2: {}".format(player1_score, player2_score))
-    tk.messagebox.showinfo("Winner!", "{} wins!".format(winner))
+    if current_player == "X":
+        player2_score += 1
+    else:
+        player1_score += 1
+    display_scores()
+    message_banner.config(text="{} wins!".format(winner))
+    message_banner.pack()
+    messagebox.showinfo("Winner!", "{} wins!".format(winner))
+
 
 def check_tie():
     # check if the game is tied and return True if it is
-    return not "" in board
+    for mark in board:
+        if mark == "":
+            return False
+    return True
+
+def display_turn():
+    # update the turn label to show the current player's turn
+    turn_label.config(text="Current Turn: Player {}".format(current_player))
+
+def display_scores():
+    # update the score label to show the current scores
+    score_label.config(text="Player 1: {}  Player 2: {}".format(player1_score, player2_score))
+
+def reset_game():
+    # reset the board, current player, and message banner
+    global board, current_player
+    board = ["", "", "", "", "", "", "", "", ""]
+    current_player = "X"
+    update_board()
+    display_turn()
+    message_banner.config(text="")
+    display_scores()
 
 def display_tie():
     # display a message declaring a tie game
-    tk.messagebox.showinfo("Tie Game!", "The game ended in a tie.")
+    message_banner.config(text="Tie game!")
+    message_banner.pack()
+    messagebox.showinfo("Tie Game", "The game ended in a tie.")
 
-def reset_game():
-    global board
-    board = ["", "", "", "", "", "", "", "", ""]
-    update_board()
-   
+# create the reset button and message banner
+reset_button = tk.Button(window, text="Reset", command=reset_game)
+reset_button.pack()
+message_banner = tk.Label(window, text="", font=("Helvetica", 16), fg="red")
 
+window.mainloop()
